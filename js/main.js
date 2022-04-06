6e83e0e024c11c837e00ae06950c2345c3e8ba9f
andualem76 <100028064+andualem76@users.noreply.github.com>
2022-04-06 22:14:26 +0300

    finishing touches

--- js/main.js
+++ js/main.js
@@ -1,25 +1,19 @@
 const collapsibles = document.querySelectorAll(".menu_btn");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
-    
- 
       var element = document.getElementById("myDIV");
       element.classList.toggle("active");
-
   })
 );
 
 const dismiss = document.querySelectorAll(".dismiss");
 dismiss.forEach((item) =>
   item.addEventListener("click", function () {
-      
- 
       var element = document.getElementById("myDIV");
       element.classList.remove("active");
 
   })
 );
-
 
   const event11 = window.addEventListener('scroll', function(e) {
     if(this.scrollY < 200) {
@@ -31,46 +25,26 @@ dismiss.forEach((item) =>
     }
   });
 
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
 
+      // Draw the chart and set the chart values
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ["Task", "Skills"],
+          ["HTML", 8],
+          ["Css", 6],
+          ["Php", 4],
+          ["JavaScript", 4],
+          ["MySql", 6],
+        ]);
 
+        // Optional; add a title and set the width and height of the chart
+        var options = { width: 650, height: 400 };
 
-
-
-
-
-
-// Load google charts
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function(){
-//     $(window).scroll(function(){
-//         if(this.scrollY > 20){
-//             $('.nav_bar').addClass("sticky")
-//         }else{
-//             $('.nav_bar').removeClass("sticky")
-//             }
-// });
-//toggle menu
-// $('.menu_btn').click(function(){
-//     $('.nav_bar .menu').toggleClass("active")
-//     $('.menu_btn i').toggleClass("active")
-// });
-// $('.dismiss').click(function(){
-//     $('.nav_bar .menu').toggleClass("active")
-//     $('.menu_btn i').toggleClass("active")
-// });
-
-// });
-
-// element = document.getElementById("myDIV");
-// element.classList.toggle("mystyle");
+        // Display the chart inside the <div> element with id="piechart"
+        var chart = new google.visualization.PieChart(
+          document.getElementById("piechart")
+        );
+        chart.draw(data, options);
+      }
